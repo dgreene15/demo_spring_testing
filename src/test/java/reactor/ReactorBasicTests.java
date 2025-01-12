273c6f319867184bfd7fdbaf68ec6041859ae534
Darren <dgreene15@yahoo.com>
2025-01-12 10:41:18 -0500

    updated reactor tests

--- src/test/java/reactor/ReactorBasicTests.java
+++ src/test/java/reactor/ReactorBasicTests.java
@@ -3,8 +3,12 @@ package reactor;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 /**
  * doOn methods
@@ -23,17 +27,28 @@ public class ReactorBasicTests {
         Example below will consume any error.
      */
     @Test
-    public void testDoOnSuccessError() {
+    public void doOnSuccess() {
 
-        Mono.just("stream data")
+        Mono<String> actual =
+                Mono.just("stream data")
                 .map(String::toUpperCase)
-                .doOnSuccess(System.out::println)
-                .doOnError(System.out::println)
-                .onErrorReturn("updated error data")
-                .subscribe(
-                        value -> System.out.println("Value: " + value),
-                        error -> System.out.println("Error: " + error)
-                );
+                .doOnSuccess(value ->
+                        assertThat(value).isEqualTo("STREAM DATA"));
+
+        StepVerifier.create(actual)
+                .expectNext("STREAM DATA")
+                .verifyComplete();
+    }
+
+    @Test
+    public void delayElement() {
+        Mono<String> actual = Mono.just("delayed string")
+                .delayElement(Duration.ofSeconds(1))
+                .map(String::toUpperCase);
+
+        StepVerifier.create(actual)
+                .expectNext("DELAYED STRING")
+                .verifyComplete();
     }
 
     /*
@@ -43,29 +58,26 @@ public class ReactorBasicTests {
     static int i = 0;
     @Test
     public void testSubscribe() {
-        Mono.delay(Duration.ofMillis(1)).map(d -> {
-            System.out.println("Value: " + d);
-            return d;
-        }).subscribe(System.out::println);
-
         MyElement myElement = new MyElement();
-
         /*
         doOnNext: side-affect for processing and continue passing elements down stream
         Variable used in lambda expression should be final or effectively final
             - works with static value
             - work if use a POJO since a reference doesn't change
          */
-        Flux.just(1, 2, 3, 4).doOnNext(
+        Flux<Integer> actual = Flux.just(1, 2, 3, 4).doOnNext(
                 element -> {
                     i = 5;
                     myElement.setName("hi");
-                    System.out.println("Hi: " + element);
                 }
-        ).flatMap(Flux::just).subscribe(System.out::println);
+        ).flatMap(Flux::just);
 
-        System.out.println("Value of i: " + i);
-        System.out.println("MyElement: " + myElement.getName());
+        StepVerifier.create(actual)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+
+        assertThat(i).isEqualTo(5);
+        assertThat(myElement.getName()).isEqualTo("hi");
     }
 
     static class MyElement {
